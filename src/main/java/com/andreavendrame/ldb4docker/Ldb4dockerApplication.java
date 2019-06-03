bd5bdf9daa6ccc94707cbf5d068b8cd903a765c0
Andrea Vendrame <andrea.risk@gmail.com>
2019-06-03 11:52:34 +0200

    Initial commit

---
+++ src/main/java/com/andreavendrame/ldb4docker/Ldb4dockerApplication.java
@@ -0,0 +1,13 @@
+package com.andreavendrame.ldb4docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ldb4dockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ldb4dockerApplication.class, args);
+    }
+
+}
